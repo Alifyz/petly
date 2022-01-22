d9497be8b7126be6edacf6089e9da25b1cbb2e3c
Alifyz Pires <alifyzdev@gmail.com>
2022-01-22 11:32:20 -0300

    fix: to google fonts

--- lib/common/theme/text_theme.dart
+++ lib/common/theme/text_theme.dart
@@ -2,20 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class PetlyTextTheme {
-  static const TextStyle h1 = TextStyle(
-    fontFamily: 'TT Norms',
+  static TextStyle h1 = GoogleFonts.lato(
     fontWeight: FontWeight.bold,
     letterSpacing: 0.9,
     fontSize: 28,
   );
-  static const TextStyle subtitle = TextStyle(
-    fontFamily: 'TT Norms',
+  static TextStyle subtitle = GoogleFonts.lato(
     fontWeight: FontWeight.w500,
     fontSize: 16,
     letterSpacing: 0.6,
   );
-  static const TextStyle headline = TextStyle(
-    fontFamily: 'TT Norms',
+  static TextStyle headline = GoogleFonts.lato(
     fontSize: 12,
     letterSpacing: 0.6,
     color: Colors.white,
