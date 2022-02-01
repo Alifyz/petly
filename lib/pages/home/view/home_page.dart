ed4d785e686ce473b00b7dfd45b4faa8f4fa03a6
Alifyz Pires <alifyzdev@gmail.com>
2022-02-01 08:50:56 -0300

    add: scrollable list

--- lib/pages/home/view/home_page.dart
+++ lib/pages/home/view/home_page.dart
@@ -10,99 +10,121 @@ class HomePage extends StatelessWidget {
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
       body: SafeArea(
-        child: Stack(
+        child: Column(
           children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: Colors.pink,
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(24),
-                ),
-              ),
-              height: mediaQuery.size.height * 0.40,
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8,
-                      vertical: 16,
-                    ),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 30,
-                          foregroundImage:
-                              NetworkImage(netwworkImageAddressExample),
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          'Hello, Alifyz Pires',
-                          style: PetlyTextTheme.subtitle.copyWith(
-                            fontSize: 14,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const Expanded(
-                          child: Align(
-                            alignment: Alignment.topRight,
-                            child: Icon(
-                              Icons.menu,
-                              color: Colors.white,
-                            ),
-                          ),
-                        )
-                      ],
+            Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(24),
                     ),
                   ),
-                  Flexible(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 36,
-                        left: 8,
-                        right: 8,
-                      ),
-                      child: SizedBox(
-                        height: 56,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
+                  height: mediaQuery.size.height * 0.40,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 16,
+                        ),
+                        child: Row(
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: Container(
-                                width: 64,
-                                height: 64,
+                            CircleAvatar(
+                              radius: 30,
+                              foregroundImage:
+                                  NetworkImage(netwworkImageAddressExample),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              'Hello, Alifyz Pires',
+                              style: PetlyTextTheme.subtitle.copyWith(
+                                fontSize: 14,
                                 color: Colors.white,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: Container(
-                                width: 64,
-                                height: 64,
-                                color: Colors.white,
+                            const Expanded(
+                              child: Align(
+                                alignment: Alignment.topRight,
+                                child: Icon(
+                                  Icons.menu,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: Container(
-                                width: 64,
-                                height: 64,
-                                color: Colors.white,
-                              ),
-                            ),
+                            )
                           ],
                         ),
                       ),
-                    ),
-                  )
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 36,
+                            left: 8,
+                            right: 8,
+                          ),
+                          child: SizedBox(
+                            height: 56,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Container(
+                                    width: 64,
+                                    height: 64,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Container(
+                                    width: 64,
+                                    height: 64,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Container(
+                                    width: 64,
+                                    height: 64,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                left: 8,
+                right: 8,
+              ),
+              child: ListView(
+                children: const [
+                  ListTile(
+                    leading: Text('Dog'),
+                  ),
+                  ListTile(
+                    leading: Text('cat'),
+                  ),
                 ],
               ),
-            )
+            ))
           ],
         ),
       ),
