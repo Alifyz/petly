8d2a28f95b965d29b895ca583ceafadb4f20953b
Alifyz Pires <alifyzdev@gmail.com>
2022-01-31 22:29:15 -0300

    add: initial layout home page

--- lib/pages/home/view/home_page.dart
+++ lib/pages/home/view/home_page.dart
@@ -1,10 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:pet_project/common/helpers.dart';
+import 'package:pet_project/common/theme/text_theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(24),
+                ),
+              ),
+              height: mediaQuery.size.height * 0.40,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 16,
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          foregroundImage:
+                              NetworkImage(netwworkImageAddressExample),
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          'Hello, Alifyz Pires',
+                          style: PetlyTextTheme.subtitle.copyWith(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Expanded(
+                          child: Align(
+                            alignment: Alignment.topRight,
+                            child: Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        bottom: 36,
+                        left: 8,
+                        right: 8,
+                      ),
+                      child: SizedBox(
+                        height: 56,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Container(
+                                width: 64,
+                                height: 64,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Container(
+                                width: 64,
+                                height: 64,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Container(
+                                width: 64,
+                                height: 64,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
