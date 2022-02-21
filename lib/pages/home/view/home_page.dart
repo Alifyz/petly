af592ce613afbd9ed81f4f69db55f113fefa00b1
Alifyz Pires <alifyzdev@gmail.com>
2022-02-21 16:57:46 -0300

    add; styles for home page

--- lib/pages/home/view/home_page.dart
+++ lib/pages/home/view/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pet_project/common/helpers.dart';
-import 'package:pet_project/common/theme/text_theme.dart';
-import 'package:pet_project/pages/home/widgets/global_action_widget.dart';
+import 'package:pet_project/pages/home/widgets/header_widget.dart';
 import 'package:pet_project/pages/home/widgets/pet_list_item_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -35,91 +33,6 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class HeaderWidget extends StatelessWidget {
-  const HeaderWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    return Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.pink,
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(24),
-            ),
-          ),
-          height: mediaQuery.size.height * 0.35,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 8,
-                  vertical: 16,
-                ),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      foregroundImage:
-                          NetworkImage(netwworkImageAddressExample),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      'Hello, Alifyz Pires',
-                      style: PetlyTextTheme.subtitle.copyWith(
-                        fontSize: 14,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Expanded(
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: Icon(
-                          Icons.menu,
-                          color: Colors.white,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 36,
-                    left: 8,
-                    right: 8,
-                  ),
-                  child: SizedBox(
-                    height: 72,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: const [
-                        GlobalActionWidget(),
-                        GlobalActionWidget(),
-                        GlobalActionWidget(),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        )
-      ],
     );
   }
 }
--- lib/pages/home/widgets/global_action_widget.dart
+++ lib/pages/home/widgets/global_action_widget.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:pet_project/common/theme/text_theme.dart';
 
 class GlobalActionWidget extends StatelessWidget {
   const GlobalActionWidget({
     Key? key,
+    required this.actionName,
+    required this.actionImage,
   }) : super(key: key);
+
+  final String actionName;
+  final AssetImage actionImage;
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +18,35 @@ class GlobalActionWidget extends StatelessWidget {
       child: Container(
         width: 64,
         height: 64,
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            )
+          ],
           color: Colors.white,
-          borderRadius: BorderRadius.all(
+          borderRadius: const BorderRadius.all(
             Radius.circular(8),
           ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: const [
-            Icon(Icons.pets),
-            Text('Dog'),
+          children: [
+            Image(
+              image: actionImage,
+              height: 24,
+              width: 24,
+            ),
+            Text(
+              actionName,
+              style: PetlyTextTheme.subtitle.copyWith(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ],
         ),
       ),
