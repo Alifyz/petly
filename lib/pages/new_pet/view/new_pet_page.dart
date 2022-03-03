b7ab5ebdaf6b7657712d95dc977fa347d1260333
Alifyz Pires <alifyzdev@gmail.com>
2022-03-03 14:06:27 -0300

    add; new layout changes

--- lib/pages/new_pet/view/new_pet_page.dart
+++ lib/pages/new_pet/view/new_pet_page.dart
@@ -5,34 +5,110 @@ class NewPetPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final navigator = Navigator.of(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.redAccent,
         elevation: 0,
-        leading: const Icon(Icons.close),
+        leading: GestureDetector(
+          onTap: navigator.pop,
+          child: const Icon(Icons.close),
+        ),
+        title: const Text('New pet'),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
         icon: const Icon(Icons.save),
         label: const Text('Save'),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Container(
-            height: 128,
-            color: Colors.redAccent,
+          Padding(
+            padding: const EdgeInsets.only(top: 16, left: 8, right: 8),
+            child: Align(
+              child: Container(
+                height: 128,
+                width: 128,
+                color: Colors.red,
+              ),
+            ),
           ),
-          TextFormField(
-            initialValue: 'Pet name',
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(),
+              ),
+              initialValue: 'Pet name',
+            ),
           ),
-          TextFormField(
-            initialValue: 'Pet Breed',
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8,
+              right: 8,
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(),
+              ),
+              initialValue: 'Pet Breed',
+            ),
           ),
-          TextFormField(
-            initialValue: 'Age',
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8,
+              right: 8,
+              top: 8,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(),
+                    ),
+                    initialValue: 'Age',
+                  ),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(),
+                    ),
+                    initialValue: 'Weight',
+                  ),
+                ),
+              ],
+            ),
           ),
-          TextFormField(
-            initialValue: 'Weight',
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(),
+              ),
+              initialValue: 'Location',
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            child: Row(
+              children: [
+                const Text('Vacinatted'),
+                Switch(
+                  value: true,
+                  onChanged: (bool newValue) {},
+                ),
+              ],
+            ),
           ),
         ],
       ),
