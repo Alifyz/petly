89db7c85883b25221c22fe9a54dcd6b116af621b
Alifyz Pires <alifyzdev@gmail.com>
2022-03-11 21:51:12 -0300

    add: cubit implementation
    Co-authored-by: Francesca <potniatheron@users.noreply.github.com>

--- lib/pages/pet_list/view/pet_list_page.dart
+++ lib/pages/pet_list/view/pet_list_page.dart
@@ -1,10 +1,54 @@
+// ignore_for_file: cast_nullable_to_non_nullable
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pet_project/pages/pet_list/cubit/pet_list_cubit.dart';
 
 class PetListPage extends StatelessWidget {
-  const PetListPage({Key? key}) : super(key: key);
+  const PetListPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final petType = ModalRoute.of(context)?.settings.arguments as String?;
+    return BlocProvider(
+      create: (_) => PetListCubit(),
+      child: PetListView(petType),
+    );
+  }
+}
+
+class PetListView extends StatefulWidget {
+  const PetListView(
+    this.petType, {
+    Key? key,
+  }) : super(key: key);
+
+  final String? petType;
+
+  @override
+  State<PetListView> createState() => _PetListViewState();
+}
+
+class _PetListViewState extends State<PetListView> {
+  @override
+  void initState() {
+    context.read<PetListCubit>().queryPets(widget.petType);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pets'),
+      ),
+      body: ListView(
+        children: [
+          Container(),
+        ],
+      ),
+    );
   }
 }
